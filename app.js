43512b25effac9044f9f087d2b7489d4d284d9af
SinBin10 <bincode10@gmail.com>
2024-08-02 18:20:16 +0530

    done with crud operations.

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.get("/create", async (req, res) => {
   res.send(user);
 });
 
+app.get("/read", async (req, res) => {
+  //find -> gives arrays of all the matched usernames
+  //findone -> gives the first one matched usernames
+  let users = await userModel.find({ username: "binay10" });
+  res.send(users);
+});
+
 app.get("/update", async (req, res) => {
   let updatedUser = await userModel.findOneAndUpdate(
     { username: "binay10" },
@@ -23,6 +30,11 @@ app.get("/update", async (req, res) => {
     { new: true }
   );
   res.send(updatedUser);
+});
+
+app.get("/delete", async (req, res) => {
+  let deletedUser = await userModel.findOneAndDelete({ username: "binay10" });
+  res.send(deletedUser);
 });
 
 app.listen(3000);
